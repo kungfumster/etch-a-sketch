b2806799c230425b72c3502bbc02614b29d1d27b
kungfumster <dannyhl88@gmail.com>
2024-03-07 17:44:50 -0600

    Completed Etch a Sketch program

--- sketch.js
+++ sketch.js
@@ -1,33 +1,97 @@
+/********************************** 
+ *LOAD DEFAULT STATE
+ **********************************/
+let numRows = 16;
+
 const container = document.querySelector(`#container`);
+buildGrid();
 
-let numColumns = 3;
-let numRows = 3;
 
-//create rows
-for (let y = 0; y < numColumns; y++) {
-    const row = document.createElement(`div`);
-    row.className = `row ${y+1}`;
-    container.appendChild(row);
+/********************************** 
+ *UPDATE DIMENSIONS
+ **********************************/
+
+const dimensionBtn = document.querySelector(`#dimension-button`);
+const updateButton = document.querySelector(`#update-button`)
+const heightInput = document.querySelector(`#height`);
+
+//Toggle popup visibilty
+function togglePopup() {
+    const box = document.querySelector(`#popup`);
+
+    if (box.style.visibility ===`hidden`){
+        box.style.visibility = `visible`;
+        dimensionBtn.setAttribute(`style`, `background-color: rgb(197 207 217); box-shadow: none; `);
+    } else {
+        box.style.visibility = `hidden`;
+        dimensionBtn.setAttribute(`style`, `background-color: none;`);
+    }   
+
+    heightInput.value = numRows;
+    heightInput.focus();
 
 }
 
-//create columns in rows
-const allRows = document.querySelectorAll(`.row`);
+dimensionBtn.addEventListener(`click`, () => { togglePopup(); });
 
-allRows.forEach((item) => {
-    for (let y = 0; y < numColumns; y++) {
-        const pixel = document.createElement('div');
-        pixel.className = `pixel ${y+1}`;
-        item.appendChild(pixel);
+//Update dimensions in popup
+function updateDimensions() {
+    numRows = Math.min(100,heightInput.value);
+    heightInput.value = numRows;
+}
+
+//Clear all child nodes
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
-})
+}
 
-const allPixels = document.SetElementsByClassName(`pixel`);
-console.log(allPixels);
-allPixels.forEach((item) => {
-    item.addEventListener(`mouseover`, () => {
-        item.setAttribute(`background-color: blue`);
+updateButton.addEventListener(`click`, () => { 
+    updateDimensions();
+    removeAllChildNodes(container);
+    buildGrid();
+    heightInput.focus();
+ });
+ 
+
+/********************************** 
+ *BUILD ETCH-A-SKETCH GRID
+ **********************************/
+
+function buildGrid() {
+    //create rows
+    for (let y = 0; y < numRows; y++) {
+        const row = document.createElement(`div`);
+        row.className = `row ${y+1}`;
+        container.appendChild(row);
+
+    }
+
+    //create columns in rows
+    const allRows = document.querySelectorAll(`.row`);
+
+    allRows.forEach((item) => {
+        for (let y = 0; y < numRows; y++) {
+            const pixel = document.createElement(`div`);
+            pixel.className = `pixel ${y+1}`;
+            item.appendChild(pixel);
+        }
+    })
+
+    const allPixels = document.querySelectorAll(`.pixel`);
+
+    //Draw on grid
+    container.addEventListener(`click`,() => {
+        allPixels.forEach((item) => {
+            item.addEventListener(`mouseover`, () => {
+                item.setAttribute(`style`, `background-color: lightslategrey`);
+            });
+        })
     });
-})
+}
+
+
+ 
 
 
